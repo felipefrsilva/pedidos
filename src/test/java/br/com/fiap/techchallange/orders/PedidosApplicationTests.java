39cd71a0a2899d714f10a7dde5d46a748fa4be8a
Eder Brito <ederpsilva@hotmail.com>
2024-05-12 08:07:43 -0300

    alter name

--- src/test/java/br/com/fiap/techchallange/orders/PedidosApplicationTests.java
+++ src/test/java/br/com/fiap/techchallange/orders/PedidosApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.fiap.techchallange.pedidos;
+package br.com.fiap.techchallange.orders;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
