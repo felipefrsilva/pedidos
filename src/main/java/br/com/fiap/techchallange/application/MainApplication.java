0feb4f212027e604e62edbb6da5e28e2a82c3437
jubel <jubelcassioverridasilva@gmail.com>
2024-05-19 16:37:24 -0300

    Setup basic module for exploring spring's database connection

--- src/main/java/br/com/fiap/techchallange/application/MainApplication.java
+++ src/main/java/br/com/fiap/techchallange/application/MainApplication.java
@@ -2,10 +2,14 @@ package br.com.fiap.techchallange.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"package br.com.fiap.techchallange.infrastructure.adapters.in"})
+@EnableJpaRepositories("br.com.fiap.techchallange.infrastructure.adapters.*")
+@ComponentScan(basePackages = {"br.com.fiap.techchallange.infrastructure.adapters.*"})
+@EntityScan("br.com.fiap.techchallange.*")
 public class MainApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/fiap/techchallange/infrastructure/adapters/out/MySQLProductRepository.java
@@ -0,0 +1,10 @@
+//package br.com.fiap.techchallange.infrastructure.adapters.out;
+//import java.sql.Connection;
+//
+//public class MySQLProductRepository {
+//    Connection conn;
+//
+//    public MySQLProductRepository() {
+//        conn = MySQLConnection.getConnection();
+//    }
+//}
