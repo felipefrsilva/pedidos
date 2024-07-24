6f2a35f6e8235d7d52b57e216f87443e20061b10
jubel <jubelcassioverridasilva@gmail.com>
2024-07-24 20:53:52 -0300

    Excluindo arquivos desnecessários

--- src/main/java/br/com/fiap/techchallange/core/usecase/inputboundary/orderpayment/IRequestOrderPaymentCodeUseCase.java
+++
@@ -1,7 +0,0 @@
-//package br.com.fiap.techchallange.core.usecase.inputboundary.orderpayment;
-//
-//import java.io.IOException;
-//
-//public interface IRequestOrderPaymentCodeUseCase {
-//    public String initializePayment(String idOrder) throws IOException;
-//}
