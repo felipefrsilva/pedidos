5be4a4514f51f247e294f2d09ce688267f483de3
Jubel Cassio <jubelcassioverridasilva@gmail.com>
2024-07-31 01:04:13 +0000

    Adding architecture png images

--- export-structurizr-to-mmd.sh
+++
@@ -1,3 +0,0 @@
-# cli documentation: https://docs.structurizr.com/cli/export
-docker pull structurizr/cli:latest
-docker run -it --rm -v ./architecture:/usr/local/structurizr structurizr/cli export -workspace workspace.json -format mermaid
